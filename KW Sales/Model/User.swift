bf34babdeacf22e10fbbe6d5251c38a2b3e52700
lukemorse <luke.morse@gmail.com>
2020-05-13 09:34:36 -0500

    Add User model

---
+++ KW Sales/Model/User.swift
@@ -0,0 +1,46 @@
+//
+//  User.swift
+//  KW Sales
+//
+//  Created by Luke Morse on 5/13/20.
+//  Copyright © 2020 Luke Morse. All rights reserved.
+//
+
+import Foundation
+
+struct User: Encodable, Hashable, Equatable {
+    var id: Int { hashValue }
+    let uid: String
+    let name: String
+    let email: String
+    let phone: String
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(uid)
+        hasher.combine(name)
+        hasher.combine(email)
+        hasher.combine(phone)
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+        case uid, name, email, phone
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(uid, forKey: .uid)
+        try container.encode(name, forKey: .name)
+        try container.encode(email, forKey: .email)
+        try container.encode(phone, forKey: .phone)
+    }
+}
+
+extension User: Decodable {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        uid = try container.decode(String.self, forKey: .uid)
+        name = try container.decode(String.self, forKey: .uid)
+        email = try container.decode(String.self, forKey: .email)
+        phone = try container.decode(String.self, forKey: .phone)
+    }
+}
